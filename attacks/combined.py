2c7a2614b4778cbb9c763e7d7313627580c481e5
danielzhang-sudo <147735541+danielzhang-sudo@users.noreply.github.com>
2024-04-19 12:19:42 +0100

    Update combined.py

--- attacks/combined.py
+++ attacks/combined.py
@@ -10,20 +10,21 @@ def run_combined(args):
     # Login into somoeone's account with SQL injection
     if sql_log:
         logged_in, session = sql_login()
-        if not logged_in:
-            print('Login failed')
-            print('Trying brute force login...')
-            # Login someone's account with brute force
-            logged_in, username, password, session = username_brute()
+        logged_in = True
+    
+    # Login someone's account with brute force
+    if not logged_in:
+        print('Login failed')
+        print('Trying brute force login...')
+        logged_in, username, password, session = username_brute()
             
-
     if logged_in:
         print('Logged in successfully!')
         # Obtain a list of products
         products_list = sql_injection(session)
 
         # Do data poisoning
-        automatic_search(session, products_list)
+        automatic_search(session, products_list, k_prod=args.k_prod, n_times=args.n_times)
     else:
         print('Failed to log in')
 
@@ -34,6 +35,8 @@ if __name__=='__main__':
     parser.add_argument('--dates_update', action='store_true', default=True)
     parser.add_argument('--from_dataset', action='store_true', default=False)
     parser.add_argument('--database', type=str, default='../unsecure_website/database.db')
+    parser.add_argument('--k_prod', type=int, default=5)
+    parser.add_argument('--n_times', type=int, default=5)
     parser.add_argument("-f", "--file", required=False)
     args = parser.parse_args()
     run_combined(args)
